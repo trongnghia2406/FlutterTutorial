a1e4efff2d84a7dbc6ba690f680746498a9c77aa
trongnghia2406 <22130186@st.hcmuaf.edu.vn>
2026-03-11 18:03:13 +0700

    lesson2: Colours, fonts

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,23 @@ runApp(MaterialApp(
       appBar: AppBar(
         title: Text('Flutter App'),
         centerTitle: true,
+        backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Text('Hello, World!'),
+        child: Text(
+          'Hello, World!',
+          style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.green[600],
+              fontFamily: 'Arial'
+            ),
+          ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
+        backgroundColor: Colors.red[600],
         child: Icon(Icons.add),
       ),
     ),
