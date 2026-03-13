193994919e900019a21b8f0eb27eafe151d145e5
trongnghia2406 <22130186@st.hcmuaf.edu.vn>
2026-03-13 21:02:46 +0700

    lesson5: Icon

---
+++ lib/lesson5.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+    home: Home(),
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Icon(
+            Icons.airport_shuttle,
+            size: 50.0,
+            color: Colors.red[600],
+          ),
+      ),
+      floatingActionButton: FloatingActionButton( 
+        onPressed: () {},
+        backgroundColor: Colors.red[600],
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
