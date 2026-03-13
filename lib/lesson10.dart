1147df04fd51a3249667ee1d206619db4eea6f2b
trongnghia2406 <22130186@st.hcmuaf.edu.vn>
2026-03-13 21:11:02 +0700

    lesson10: padding widget

---
+++ lib/lesson10.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+    home: Home(),
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text('Hello World'),
+      ),
+      floatingActionButton: FloatingActionButton( 
+        onPressed: () {},
+        backgroundColor: Colors.red[600],
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
