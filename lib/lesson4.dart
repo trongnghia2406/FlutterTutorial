879f2240dc677d5cd02e1612ce002669778dea95
trongnghia2406 <22130186@st.hcmuaf.edu.vn>
2026-03-13 21:00:04 +0700

    lesson4: images and assets

---
+++ lib/lesson4.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+    home: Home(),
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Image.asset('assets/images/keeramondo.jpg'),
+        // child: Image.network('https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba'), -> ảnh mạng
+        // child: Image( :cách khác để hiển thị ảnh
+        //     image: NetworkImage('https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba'),
+        //     image: AssetImage('assets/images/keeramondo.jpg'),
+        //   ),
+        ),
+      floatingActionButton: FloatingActionButton( 
+        onPressed: () {},
+        backgroundColor: Colors.red[600],
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
