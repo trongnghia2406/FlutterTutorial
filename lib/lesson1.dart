6f49081636edb9ac60e5fd9bba20c49bb53b3580
trongnghia2406 <22130186@st.hcmuaf.edu.vn>
2026-03-13 20:51:03 +0700

    lesson1: scafford, appbar widget

---
+++ lib/lesson1.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('Hello Flutter!'),
+      ),
+      floatingActionButton: FloatingActionButton( 
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    ),
+));
