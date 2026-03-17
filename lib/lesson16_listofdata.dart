cb57fe38fbc2cd0e8e4c9c6b7aa4053ab6a3cb8e
trongnghia2406 <22130186@st.hcmuaf.edu.vn>
2026-03-17 09:32:17 +0700

    lesson16: list of data

---
+++ lib/lesson16_listofdata.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: QuoteList(),
+  ));
+}
+
+class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
+
+  @override
+  State<QuoteList> createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<String> quotes = [
+    'GAM BangThan', 'GG BangThan', 'GG K', 'GG Long', 'GG Miu', 'GG TiSon', 'GG ThuHang'
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        title: Text('Awesome Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Column(
+        children: quotes.map((quote) => Text(quote)).toList(),
+      ),
+    );
+  }
+}
+
